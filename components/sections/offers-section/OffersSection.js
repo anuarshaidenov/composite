569fd6a065cf3634fe599bfd51083fab10004927
Anuar Shaidenov <anuarshaidenov@gmail.com>
2022-06-27 11:39:58 +0600

    Refactor offers section

--- components/sections/offers-section/OffersSection.js
+++ components/sections/offers-section/OffersSection.js
@@ -5,6 +5,41 @@ import InstitutionIcon from '../../../assets/offers-section/instituotion-icon.sv
 import GraduationIcon from '../../../assets/offers-section/graduation-icon.svg';
 import ScholarshipIcon from '../../../assets/offers-section/scholarship-icon.svg';
 
+const offers = [
+  {
+    id: 1,
+    title: 'The best place to study',
+    icon: PlacesIcon,
+    iconColor: 'bg-[#0A11E0]',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 2,
+    title: 'Top institution for chosen programs',
+    icon: InstitutionIcon,
+    iconColor: 'bg-colorSecondary',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 3,
+    title: 'The best program to enter desired field',
+    icon: GraduationIcon,
+    iconColor: 'bg-[#3ECDAB]',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    id: 4,
+    title: 'Get a scholarship',
+    icon: ScholarshipIcon,
+    iconColor: 'bg-[#41C04E]',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
+
 const OffersSection = () => {
   return (
     <section className="py-20">
@@ -17,82 +52,27 @@ const OffersSection = () => {
           eiusmod tempor incididunt ut labore et dolore magna aliqua
         </p>
         <ul className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          <li className="offer-item flex flex-col items-start sm:flex-row gap-4 md:gap-8">
-            <div className="bg-[#0A11E0] p-3 w-16 h-16 md:w-20 md:h-20 rounded-2xl flex-none">
-              <div className="relative w-full h-full">
-                <Image src={PlacesIcon} alt="Places Icon" layout="fill" />
-              </div>
-            </div>
-            <div>
-              <h4 className="font-semibold text-colorStone mb-3 text-2xl">
-                The best place to study
-              </h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
-              </p>
-            </div>
-          </li>
-          <li className="offer-item flex flex-col items-start sm:flex-row gap-4 md:gap-8">
-            <div className="bg-colorSecondary p-3 w-16 h-16 md:w-20 md:h-20 rounded-2xl flex-none">
-              <div className="relative w-full h-full">
-                <Image
-                  src={InstitutionIcon}
-                  alt="Institution Icon"
-                  layout="fill"
-                />
-              </div>
-            </div>
-            <div>
-              <h4 className="font-semibold text-colorStone mb-3 text-2xl">
-                Top institution for chosen programs
-              </h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
-              </p>
-            </div>
-          </li>
-          <li className="offer-item flex flex-col items-start sm:flex-row gap-4 md:gap-8">
-            <div className="bg-[#3ECDAB] p-3 w-16 h-16 md:w-20 md:h-20 rounded-2xl flex-none">
-              <div className="relative w-full h-full">
-                <Image
-                  src={GraduationIcon}
-                  alt="Graduation Icon"
-                  layout="fill"
-                />
-              </div>
-            </div>
-            <div>
-              <h4 className="font-semibold text-colorStone mb-3 text-2xl">
-                The best program to enter desired field
-              </h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
-              </p>
-            </div>
-          </li>
-          <li className="offer-item flex flex-col items-start sm:flex-row gap-4 md:gap-8">
-            <div className="bg-[#41C04E] p-3 w-16 h-16 md:w-20 md:h-20 rounded-2xl flex-none">
-              <div className="relative w-full h-full">
-                <Image
-                  src={ScholarshipIcon}
-                  alt="Scholarship Icon"
-                  layout="fill"
-                />
-              </div>
-            </div>
-            <div>
-              <h4 className="font-semibold text-colorStone mb-3 text-2xl">
-                Get a scholarship
-              </h4>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
-              </p>
-            </div>
-          </li>
+          {offers &&
+            offers.map((offer) => (
+              <li
+                key={offer.id}
+                className="offer-item flex flex-col items-start sm:flex-row gap-4 md:gap-8"
+              >
+                <div
+                  className={`${offer.iconColor} p-3 w-16 h-16 md:w-20 md:h-20 rounded-2xl flex-none`}
+                >
+                  <div className="relative w-full h-full">
+                    <Image src={offer.icon} alt={offer.title} layout="fill" />
+                  </div>
+                </div>
+                <div>
+                  <h4 className="font-semibold text-colorStone mb-3 text-2xl">
+                    {offer.title}
+                  </h4>
+                  <p>{offer.description}</p>
+                </div>
+              </li>
+            ))}
         </ul>
       </div>
     </section>
